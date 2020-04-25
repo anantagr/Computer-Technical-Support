fff9a544f73486c7956e7d7e798ed408f0943f6e
Anant Agarwal <56859434+anantagr@users.noreply.github.com>
2020-04-25 05:29:33 -0600

    Add files via upload

---
+++ GUI Technical Support/mainForm.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace GUI_Technical_Support
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.computerBeepCheckBox = new System.Windows.Forms.CheckBox();
+            this.DiscdriveSpinsCheckBox = new System.Windows.Forms.CheckBox();
+            this.outputButton = new System.Windows.Forms.Button();
+            this.quitButton = new System.Windows.Forms.Button();
+            this.resultTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // computerBeepCheckBox
+            // 
+            this.computerBeepCheckBox.AutoSize = true;
+            this.computerBeepCheckBox.Location = new System.Drawing.Point(135, 44);
+            this.computerBeepCheckBox.Name = "computerBeepCheckBox";
+            this.computerBeepCheckBox.Size = new System.Drawing.Size(204, 29);
+            this.computerBeepCheckBox.TabIndex = 0;
+            this.computerBeepCheckBox.Text = "Computer Beeps";
+            this.computerBeepCheckBox.UseVisualStyleBackColor = true;
+            this.computerBeepCheckBox.CheckedChanged += new System.EventHandler(this.checkBoxChanged);
+            // 
+            // DiscdriveSpinsCheckBox
+            // 
+            this.DiscdriveSpinsCheckBox.AutoSize = true;
+            this.DiscdriveSpinsCheckBox.Location = new System.Drawing.Point(135, 93);
+            this.DiscdriveSpinsCheckBox.Name = "DiscdriveSpinsCheckBox";
+            this.DiscdriveSpinsCheckBox.Size = new System.Drawing.Size(193, 29);
+            this.DiscdriveSpinsCheckBox.TabIndex = 1;
+            this.DiscdriveSpinsCheckBox.Text = "Discdrive Spins";
+            this.DiscdriveSpinsCheckBox.UseVisualStyleBackColor = true;
+            this.DiscdriveSpinsCheckBox.CheckedChanged += new System.EventHandler(this.checkBoxChanged);
+            // 
+            // outputButton
+            // 
+            this.outputButton.Location = new System.Drawing.Point(121, 145);
+            this.outputButton.Name = "outputButton";
+            this.outputButton.Size = new System.Drawing.Size(218, 48);
+            this.outputButton.TabIndex = 2;
+            this.outputButton.Text = "What should I do?";
+            this.outputButton.UseVisualStyleBackColor = true;
+            this.outputButton.Click += new System.EventHandler(this.outputButton_Click);
+            // 
+            // quitButton
+            // 
+            this.quitButton.Location = new System.Drawing.Point(164, 338);
+            this.quitButton.Name = "quitButton";
+            this.quitButton.Size = new System.Drawing.Size(110, 41);
+            this.quitButton.TabIndex = 3;
+            this.quitButton.Text = "Quit";
+            this.quitButton.UseVisualStyleBackColor = true;
+            this.quitButton.Click += new System.EventHandler(this.quitButton_Click);
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultTextBox.ForeColor = System.Drawing.Color.Red;
+            this.resultTextBox.Location = new System.Drawing.Point(36, 228);
+            this.resultTextBox.Multiline = true;
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.ReadOnly = true;
+            this.resultTextBox.Size = new System.Drawing.Size(391, 92);
+            this.resultTextBox.TabIndex = 4;
+            this.resultTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(544, 450);
+            this.Controls.Add(this.resultTextBox);
+            this.Controls.Add(this.quitButton);
+            this.Controls.Add(this.outputButton);
+            this.Controls.Add(this.DiscdriveSpinsCheckBox);
+            this.Controls.Add(this.computerBeepCheckBox);
+            this.Name = "mainForm";
+            this.Text = "Technical Support";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox computerBeepCheckBox;
+        private System.Windows.Forms.CheckBox DiscdriveSpinsCheckBox;
+        private System.Windows.Forms.Button outputButton;
+        private System.Windows.Forms.Button quitButton;
+        private System.Windows.Forms.TextBox resultTextBox;
+    }
+}
+
---
+++ GUI Technical Support/mainForm.cs
@@ -0,0 +1,75 @@
+﻿// Program Name:    GUI Technical Support
+
+// Author:          Anant Agarwal
+
+// Date:            10 April 20
+
+//Description:      Mini-Project 4 Part 2
+//                      The intent of this application is guide user what to do with a malfunctiong computer.
+
+//Information provided by the user:
+//                  1) If computer beeps ------ Y or N via checkboxes
+//                  1) If discdrive beeps ----- Y or N via checkboxes
+
+//There were no compilation errors in the program
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI_Technical_Support
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+        
+        //Clicking the Quit button will exit the application.
+        private void quitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        //Based on user's selection, we are determing the problem and displaying it in the textbox.
+        //We are using if-else ladder to do the same
+        private void outputButton_Click(object sender, EventArgs e)
+        {
+            if (computerBeepCheckBox.Checked )
+            {
+                if(DiscdriveSpinsCheckBox.Checked)
+                {
+                    resultTextBox.Text = "Bring your computer in to repair center";
+                }
+                else
+                {
+                    resultTextBox.Text = "Check drive cables";
+                }
+            }
+            else
+            {
+                if (DiscdriveSpinsCheckBox.Checked)
+                {
+                    resultTextBox.Text = "Check speaker wires";
+                }
+                else
+                {
+                    resultTextBox.Text = "Call Tech Support";
+                }
+            }
+        }
+
+        //Everytime the user changes it choices, the display message is reset to blank.
+        private void checkBoxChanged(object sender, EventArgs e)
+        {
+            resultTextBox.Text = "";
+        }
+    }
+}
